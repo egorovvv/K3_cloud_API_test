fdcdb19b6a767d6768c9f5055d1f9c3ded9d263c
nepoimaesh <evv93@mail.ru>
2020-01-14 10:53:54 +0800

    qwe

--- venv/workfile.py
+++ venv/workfile.py
@@ -1,15 +1,15 @@
-import requests
-
-api_ep_auth = "http://hzkdeey.gnway.cc:9080/K3Cloud/Kingdee.BOS.WebApi.ServicesStub.AuthService.ValidateUser.common.kdsvc"
-acctID = '558cbb01bfc79b'
-username = 'SPLAT0001'
-password = '888888'
-lcid = '1033'
-
-pl_auth = dict(acctID = acctID, username = username, password = password, lcid = lcid)
-
-rq_auth = requests.get(api_ep_auth, params = pl_auth)
-
-print(rq_auth)
+# import requests
+#
+# api_ep_auth = "http://hzkdeey.gnway.cc:9080/K3Cloud/Kingdee.BOS.WebApi.ServicesStub.AuthService.ValidateUser.common.kdsvc"
+# acctID = '558cbb01bfc79b'
+# username = 'SPLAT0001'
+# password = '888888'
+# lcid = '1033'
+#
+# pl_auth = dict(acctID = acctID, username = username, password = password, lcid = lcid)
+#
+# rq_auth = requests.get(api_ep_auth, params = pl_auth)
+#
+# print(rq_auth)
 
 print("Complete!")
